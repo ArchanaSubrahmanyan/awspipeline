7f11bc670d2d6c4019d47d87d33a65fc3f086e60
ArchanaSubrahmanyan <245243@ust.com>
2023-06-02 16:48:33 +0530

    first commit

---
+++ src/test/java/com/ust/pipeline/AwspipelineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ust.pipeline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwspipelineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
